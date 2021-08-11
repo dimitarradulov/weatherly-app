6bf9ca831a2f27de98029ea5edf27d88ddf82e80
Dimitar Radulov <dimitaradulov@gmail.com>
2021-08-11 13:13:41 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,48 @@
+'use script';
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
+const API_KEY = 'a8d61c16d66050cedd3700780014c989';
+const baseUrl = 'https://api.openweathermap.org/';
+
+const cityInputEl = document.getElementById('city');
+const buttonEl = document.querySelector('.btn');
+const mainEl = document.querySelector('.main');
+
+const weatherInfoTemplate = (city) => html`
+  <div class="card border-primary mb-3" style="max-width: 20rem;">
+    <div class="card-header">Weather Info - ${city.name}</div>
+    <div class="card-body">
+      <h4 class="card-title">${city.weather[0].main}</h4>
+      <p class="card-text d-flex flex-column">
+        <span>Current Temp: ${city.main.temp}</span>
+        <span>Min. Temp: ${city.main.temp_min}</span>
+        <span>Max. Temp: ${city.main.temp_max}</span>
+      </p>
+    </div>
+  </div>
+`;
+
+const getCity = (ev) => {
+  ev.preventDefault();
+
+  const radioButtons = document.querySelectorAll('input[name="metric-choice"]');
+
+  let selectedValue;
+
+  for (const btn of radioButtons) {
+    if (btn.checked) {
+      selectedValue = btn.value;
+      break;
+    }
+  }
+
+  fetch(
+    `${baseUrl}/data/2.5/weather?q=${cityInputEl.value}&appid=${API_KEY}&units=${selectedValue}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      render(weatherInfoTemplate(data), mainEl);
+    });
+};
+
+buttonEl.addEventListener('click', getCity);
